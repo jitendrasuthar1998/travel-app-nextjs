4796002d619ca3c931aec4f432c201270bbc86c2
jitendrasuthar <jitendrasuthar995@gmail.com>
2024-03-28 01:26:24 +0530

    completed Guide section

--- components/Camp.tsx
+++ components/Camp.tsx
@@ -52,7 +52,7 @@ const CampSite = ({
 
 const Camp = () => {
   return (
-    <section className='border-2 border-green-500 2xl:max-container relative flex flex-col py-10 lg:mb-10 lg:py-20 xl:mb-20'>
+    <section className='2xl:max-container relative flex flex-col py-10 lg:mb-10 lg:py-20 xl:mb-20'>
       <div className='hide-scrollbar flex h-[340px] w-full items-start justify-start gap-8 overflow-auto lg:h-[400px] xl:h-[640px]'>
         <CampSite
           backgroundImg='bg-bg-img-1'
